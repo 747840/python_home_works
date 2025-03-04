0cd100d0a5e511a5e1cef90c0641f9b96ba84d66
Aleksandra Matveeva <al.matv.al@gmail.com>
2025-03-04 08:56:37 +0500

    Домашка1

--- 1_lesson/lesson_1_task_5.py
+++ 1_lesson/lesson_1_task_5.py
@@ -2,5 +2,14 @@ def print_number(num):
     print(num)
 
 
-for _ in range(11):
-    print_number("88005553535")
+print_number(88005553535)
+print_number(88005553535)
+print_number(88005553535)
+print_number(88005553535)
+print_number(88005553535)
+print_number(88005553535)
+print_number(88005553535)
+print_number(88005553535)
+print_number(88005553535)
+print_number(88005553535)
+print_number(88005553535)
