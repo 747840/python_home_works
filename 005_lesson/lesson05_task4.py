cd5449fd4374805b54f4dc82375b6a4945cb2db2
Aleksandra Matveeva <al.matv.al@gmail.com>
2025-04-06 18:42:00 +0500

    Домашка5

---
+++ 005_lesson/lesson05_task4.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+
+driver = webdriver.Firefox()
+driver.get("http://the-internet.herokuapp.com/login")
+sleep(2)
+input_field = driver.find_element(By.ID, "username")
+input_field.send_keys("tomsmith")
+sleep(2)
+input_field = driver.find_element(By.ID, "password")
+input_field.send_keys("SuperSecretPassword!")
+sleep(2)
+button = driver.find_element(By.CSS_SELECTOR, ".fa-2x")
+button.click()
+sleep(2)
+driver.quit()
